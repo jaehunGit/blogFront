eea7657c8de86d4efacfab5cfed39f4fc22e381f
jaehunGit <ghty6323@gmail.com>
2024-05-29 23:17:48 +0900

    네비게이션바 생성

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GlobalStyles from "./styles/GlobalStyles.styles";
 import MainContainer from "./styles/MainContainer.styles";
 import SubContainer1 from "./styles/SubContainer1.Styles";
 import SubContainer2 from "./styles/SubContainer2.Styles";
+import NavigationBar from "./styles/NavigationBar.styles";
 
 function App() {
   return (
@@ -12,6 +13,11 @@ function App() {
         <SubContainer1></SubContainer1>
         <SubContainer2></SubContainer2>
       </MainContainer>
+      <NavigationBar>
+        {/* <div style={{ width: "100%", height: "50px" }}></div>
+        <div style={{ width: "100%", height: "50px" }}></div>
+        <div style={{ width: "100%", height: "50px" }}></div> */}
+      </NavigationBar>
     </>
   );
 }
---
+++ src/styles/NavigationBar.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const NavigationBar = styled.div`
+  width: 60px;
+  position: fixed;
+  top: 6vh;
+  right: 4.5vw;
+  background: #fff;
+  border-radius: 35px;
+  box-shadow: 0 0 30px -5px rgb(0 0 0 / 15%);
+`;
+
+export default NavigationBar;
